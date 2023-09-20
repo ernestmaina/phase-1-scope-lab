347533cc8a4245657c8841e219981a4b3bee7e50
ErnestMaina <you@example.com>
2023-09-20 23:31:44 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Write your solution in this file!
+
+var customerName;
+
+function upperCaseCustomerName(){
+    if (typeof customerName === 'string') {
+        customerName = customerName.toUpperCase();
+}
+}
+
+upperCaseCustomerName();
+
+console.log(customerName);
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+  
+  setBestCustomer();
+  
+
+  console.log(bestCustomer); 
+
+
+
+var bestCustomer = 'not bob';
+
+function overwriteBestCustomer() {
+  bestCustomer = 'new best customer';
+}
+
+
+overwriteBestCustomer();
+
+
+console.log(bestCustomer); 
+
+const leastFavoriteCustomer="Alex"
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="Jane"
+}
+
+changeLeastFavoriteCustomer();
+
+console.log(leastFavoriteCustomer);
+
+
+ 
+
+
+
+  
+
